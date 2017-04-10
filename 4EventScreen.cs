72ab984699751b626f8b6c3c80acfe1049612bcd
dsdude123 <csmediaseattle@gmail.com>
2017-04-10 13:20:56 -0700

    Started work on event screen (4-choice)

---
+++ 4EventScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GOL_UIPrototype
+{
+    public partial class _4EventScreen : Form
+    {
+        public _4EventScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
