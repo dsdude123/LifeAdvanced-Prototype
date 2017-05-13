2c8a8dad0ae326686ec5e3b2b98a9fce6c4984ab
Conner Starkel <csmediaseattle@gmail.com>
2017-05-13 16:07:07 -0700

    Added demo warning

--- GameSetup.cs
+++ GameSetup.cs
@@ -67,6 +67,7 @@ namespace LifeAdvanced
             {
                 throw new ArgumentOutOfRangeException();
             }
+            MessageBox.Show("For demo purposes, you will start at tile 20 and play until tile 26 (age 16-55).", "Demo Version", MessageBoxButtons.OK,MessageBoxIcon.Warning);
             Form newGame = new TurnScreen();
             newGame.Show();
             this.Hide();
