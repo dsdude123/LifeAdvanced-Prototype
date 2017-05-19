141a8802d68aa3e7c6238150d6eb1bb41e107fe2
Conner Starkel <csmediaseattle@gmail.com>
2017-05-18 20:58:54 -0700

    Added MMA event

--- Events/GFCheat.cs
+++ Events/GFCheat.cs
@@ -5,7 +5,69 @@ using System.Text;
 
 namespace LifeAdvanced.Events
 {
-    class GFCheat
+    class GFCheat : Event
     {
+        public GFCheat()
+        {
+            initEvent();
+        }
+
+        public override void initEvent()
+        {
+            this.eventName = "Cheating";
+            this.eventText = "You arrive at your girlfriend's house and enter without knocking and discover her hooking up with your best friend. What do you do find a new girlfriend and a new best friend or kick one of them to the curb and remain cool with the other one.";
+            this.choice1Text = "Stay at Scene";
+            this.choice2Text = "Flee";
+            this.phaseCodes = new int[8] { 2, 3, 4, 5, 6, 7, 8, 9 };
+            this.effectChooser = new Random();
+        }
+        public override void choice1()
+        {
+            int c = effectChooser.Next(1, 4);
+            if (c == 1)
+            {
+                this.resultText = "You exchange insurance information.";
+                this.effectText = "-200 Money";
+            }
+            else if (c == 2)
+            {
+                this.resultText = "A police officer on scene finds that you were at fault for the accident and tickets you for it.";
+                this.effectText = "-500 Money";
+            }
+            else if (c == 3)
+            {
+                this.resultText = "The damage to the vehicles ends up being so minimal that the other driver declines to exchange insurance information and lets you go on your way.";
+                this.effectText = "No effect";
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+        }
+        public override void choice2()
+        {
+            int c = effectChooser.Next(1, 4);
+            if (c == 1)
+            {
+                this.resultText = "Your attempt to flee seems sucessful at first, but the next morning you are awoken by police at your front door and arrested for hit and run.";
+                this.effectText = "Skip 2 turns. -1000 Money";
+            }
+            else if (c == 2)
+            {
+                this.resultText = "Your attempt to flee is quickly stopped by the other driver. You are injured in the event.";
+                this.effectText = "-3 Health";
+            }
+            else if (c == 3)
+            {
+                this.resultText = "You sucessfuly escape the scene of the accident.";
+                this.effectText = "No effect";
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+        }
     }
 }
---
+++ Events/MMAWeed.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LifeAdvanced.Events
+{
+    class MMAWeed : Event
+    {
+        public MMAWeed()
+        {
+            initEvent();
+        }
+
+        public override void initEvent()
+        {
+            this.eventName = "Bad Influences";
+            this.eventText = "You're training to become a MMA fighter. All your friends smoke marijuana heavily everyday. You feel it will be ruining your career to remain friends with them. Do you remain friends and potentially abandon your MMA dreams or do you part ways and persure your dream.";
+            this.choice1Text = "Stay with friends";
+            this.choice2Text = "Part ways";
+            this.phaseCodes = new int[1] { 2};
+            this.effectChooser = new Random();
+        }
+        public override void choice1()
+        {
+            int c = effectChooser.Next(1, 4);
+            if (c == 1)
+            {
+                this.resultText = "You decide to stay friends and later give in to peer pressure and begin smokin marijuana.";
+                this.effectText = "+3 Happiness";
+            }
+            else if (c == 2)
+            {
+                this.resultText = "You decide to stay friends but later develop a severe addiction to marijuana.";
+                this.effectText = "+3 Happiness -2 Physical Wellbeing";
+            }
+            else if (c == 3)
+            {
+                this.resultText = "You abandon your dreams and remain friends.";
+                this.effectText = "No effect";
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+        }
+        public override void choice2()
+        {
+            int c = effectChooser.Next(1, 4);
+            if (c == 1)
+            {
+                this.resultText = "You part ways with your friends and succesfully become an MMA fighter.";
+                this.effectText = "Increase income level by 1. -1 Happiness";
+            }
+            else if (c == 2)
+            {
+                this.resultText = "You part ways with your friends but unfortinutely never achieve your MMA goals.";
+                this.effectText = "-3 Happiness";
+            }
+            else if (c == 3)
+            {
+                this.resultText = "You part ways with your friends.";
+                this.effectText = "-2 Happiness";
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+        }
+    }
+}
