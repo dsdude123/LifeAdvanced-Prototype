fa28b3e1562dea2757a0eedebce227470a1d9fe4
Conner Starkel <csmediaseattle@gmail.com>
2017-05-27 22:25:47 -0700

    Bugfix

--- Global.cs
+++ Global.cs
@@ -69,6 +69,7 @@ namespace LifeAdvanced
             cardDatabase.Add(new CardEvents.RadioContest());
             cardDatabase.Add(new CardEvents.SemiTruckCrash());
             cardDatabase.Add(new CardEvents.Snowstorm());
+            cardDatabase.Add(new CardEvents.Windstorm());
         }
 
 
